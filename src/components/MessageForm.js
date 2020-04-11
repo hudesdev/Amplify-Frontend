9f2645f3d66fd8472aa84c5c258489a433ad5169
hudesdev <hudes0112@gmail.com>
2020-04-11 00:15:01 -0400

    Initial commit

---
+++ src/components/MessageForm.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+
+const MessageForm = () => {
+    const { handleSubmit, register, errors, reset } = useForm();
+
+    const onSubmit = (values, e) => {
+        console.log(values);
+        axios.post("https://wraith-test.herokuapp.com/api/users", values)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+        e.target.reset();
+    };
+
+    return (
+        <div className = "messageform">
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+            <TextField
+            variant="outlined"
+            name="name"
+            type="text"
+            inputRef={register({
+              required: "Please enter a message.",
+              validate: value =>
+              value.length <= 20 || "No more than 20 characters."
+            })}
+            placeholder="New Message"
+          />
+          <br/>
+          <p>{errors.name && errors.name.message}</p>
+          <Button type="submit" variant = "outlined" color = "primary">
+            Send
+          </Button>
+
+            </form>
+            </div>
+    )
+};
+
+export default MessageForm;
---
+++ src/components/Messages.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import MessageForm from "./MessageForm";
+
+const Messages = () => {
+    const [messages, setMessages] = useState([]);
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        const timer = setInterval(() => {      
+            setSeconds(seconds + 1);
+        }, 2000);
+
+        return () => clearInterval(timer);
+    });
+
+    useEffect(() => {
+        axios.get("https://wraith-test.herokuapp.com/api/users")
+        .then(res => setMessages(res.data.slice(res.data.length - 7, res.data.length)))
+        .catch(err => console.log(err));
+    }, [seconds]);
+
+    return (
+        <div>
+            {messages.map(message => (
+                <div key = {message.id} className = "message">
+                <p>{message.name}</p>
+                </div>
+            ))}
+            <MessageForm />
+        </div>
+    );
+};
+
+export default Messages;

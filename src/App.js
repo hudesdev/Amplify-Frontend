a9b6f015b1cdd1f3a84aca915e3139f55c4ab992
hudesdev <hudes0112@gmail.com>
2020-04-11 19:46:40 -0400

    Change name to Z Chat

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <p>Messages</p>
+     <p>Z Chat</p>
       </header>
       <div className = "App-body">
       <div className = "messagescontainer">

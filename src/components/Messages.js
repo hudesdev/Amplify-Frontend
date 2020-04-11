1dc0b89d3f23fa59e60450f364e869ae65cc1856
hudesdev <hudes0112@gmail.com>
2020-04-11 18:49:53 -0400

    Add loading status wheel

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MessageForm from "./MessageForm";
 import HighlightOffOutlinedIcon from '@material-ui/icons/HighlightOffOutlined';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const Messages = () => {
     const [messages, setMessages] = useState([]);
     const [seconds, setSeconds] = useState(0);
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         const timer = setInterval(() => {      
@@ -15,15 +17,26 @@ const Messages = () => {
         return () => clearInterval(timer);
     });
 
+    function sleeper(ms) {
+        return function(x) {
+          return new Promise(resolve => setTimeout(() => resolve(x), ms));
+        };
+      }
+
     useEffect(() => {
         axios.get("https://wraith-test.herokuapp.com/api/users")
-        .then(res => res.data.length >= 7 ? setMessages(res.data.slice(res.data.length - 7, res.data.length)) : setMessages(res.data))
+        // .then(sleeper(2000))
+        .then(res => {
+            res.data.length >= 7 ? setMessages(res.data.slice(res.data.length - 7, res.data.length)) : setMessages(res.data);
+            setLoaded(true);
+        })
         .catch(err => console.log(err));
     }, [seconds]);
 
     return (
         <div>
-            {messages.map(message => (
+            {!loaded ? <CircularProgress color="secondary" /> :
+            messages.map(message => (
                 <div key = {message.id} className = "message">
                      <p>{message.name}</p>
                 <HighlightOffOutlinedIcon onClick = {() => axios.delete(`https://wraith-test.herokuapp.com/api/users/${message.id}`)} color = "secondary" className = "delete"></HighlightOffOutlinedIcon>

cc347bb299040a2737de1e0f41fa56c731bf3b47
hudesdev <hudes0112@gmail.com>
2020-04-11 23:09:07 -0400

    New delete icon with hover/click effect

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MessageForm from "./MessageForm";
-import HighlightOffOutlinedIcon from '@material-ui/icons/HighlightOffOutlined';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const Messages = () => {
     const [messages, setMessages] = useState([]);
@@ -39,7 +40,9 @@ const Messages = () => {
             messages.map(message => (
                 <div key = {message.id} className = "message">
                      <p>{message.name}</p>
-                <HighlightOffOutlinedIcon onClick = {() => axios.delete(`https://wraith-test.herokuapp.com/api/users/${message.id}`)} color = "secondary" className = "delete"></HighlightOffOutlinedIcon>
+                <IconButton aria-label="delete" color = "secondary" size = "small">
+                    <DeleteIcon className = "delete" onClick = {() => axios.delete(`https://wraith-test.herokuapp.com/api/users/${message.id}`)} />
+                    </IconButton>
                 </div>
             ))}
             <MessageForm />

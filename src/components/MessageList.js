c95b08f16dbeb507ee6e29bbd97df0510f488e5b
hudesdev <hudes0112@gmail.com>
2020-04-15 17:13:20 -0400

    Create MessageList component for mapping logic

---
+++ src/components/MessageList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import moment from "moment";
+import "moment-timezone";
+
+const MessageList = ({ messages }) => {
+    return (
+        <div>
+              {messages.map(message => (
+                <div key = {message.id} className = "message">
+                    <div className = "timestamp">
+                    <p>{moment.tz(message.created_at, "UTC").clone().tz(moment.tz.guess()).format('LT')}</p>
+                    </div>
+                    <div className = "username">
+                    <p id = "writer">{message.username}:</p>
+                    </div>
+                     <p id = "usermessage">{message.message}</p>
+                </div>))}
+        </div>
+    )
+}
+
+export default MessageList;
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,11 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MessageForm from "./MessageForm";
 import CircularProgress from '@material-ui/core/CircularProgress';
-// import IconButton from '@material-ui/core/IconButton';
-// import DeleteIcon from '@material-ui/icons/Delete';
-import moment from "moment";
-import "moment-timezone";
 import UserForm from "./UserForm";
+import MessageList from "./MessageList";
 
 const Messages = () => {
     const [messages, setMessages] = useState([]);
@@ -28,7 +25,8 @@ const Messages = () => {
         axios.get("https://chat-app-z.herokuapp.com/api/messages")
         // .then(sleeper(4000))
         .then(res => {
-            res.data.length >= 6 ? setMessages(res.data.slice(res.data.length - 6, res.data.length)) : setMessages(res.data);
+            res.data.length >= 50 ? setMessages(res.data.slice(res.data.length - 50, res.data.length)) : 
+            setMessages(res.data);
             setLoaded(true);
         })
         .catch(err => console.log(err));
@@ -45,22 +43,7 @@ const Messages = () => {
             {window.localStorage.getItem("username") ? <h3 id = "welcome" >Welcome, {window.localStorage.getItem("username")}! <br/> <button onClick = {() => localStorage.removeItem("username")} className = "signout">Not {window.localStorage.getItem("username")}?</button></h3> : 
             <div className = "userform"> <UserForm /> </div>}
             {!loaded ? <CircularProgress color="secondary" /> :
-            messages.map(message => (
-                <div key = {message.id} className = "message">
-                    <div className = "timestamp">
-                    <p>{moment.tz(message.created_at, "UTC").clone().tz(moment.tz.guess()).format('LT')}</p>
-                    </div>
-                    <div className = "username">
-                    <p id = "writer">{message.username}:</p>
-                    </div>
-                     <p id = "usermessage">{message.message}</p>
-                     <div className = "delete">
-                 {/* <IconButton aria-label="delete" color = "secondary" size = "small">
-                     <DeleteIcon onClick = {() => axios.delete(`https://chat-app-z.herokuapp.com/api/messages/${message.id}`)} />
-                     </IconButton> */}
-                     </div>
-                </div>
-            ))}
+            <MessageList messages = {messages} />}
             {typing && loaded ? <p id = "typing">&nbsp;</p> : <p id = "typing">&nbsp;</p>}
             <MessageForm userid = {window.localStorage.getItem("username")} />
         </div>
